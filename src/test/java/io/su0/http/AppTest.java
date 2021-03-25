94d2fa47cfae2f8bc7bf6fdabee69f3869e5137d
Kirill Sulim <kirillsulim@gmail.com>
2021-03-25 23:30:59 +0500

    Add scaffold code

---
+++ src/test/java/io/su0/http/AppTest.java
@@ -0,0 +1,20 @@
+package io.su0.http;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
